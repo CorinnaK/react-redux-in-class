fc5656095af8ab5d4271b4e0719b258ab266eac3
CorinnaK <corinna.kelly@outlook.com>
2020-07-24 11:24:44 -0600

    added new Hello component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
 function App() {
   return (
@@ -16,9 +17,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          REacting to REact
         </a>
       </header>
+      <Hello/>
     </div>
   );
 }
---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Hello extends React.Component {
+    render ()
+    {
+        return (
+            <>
+            <h2>My First Component</h2>
+            <p>Hello, World!</p>
+            </>
+        );
+    }
+}
+export default Hello;
