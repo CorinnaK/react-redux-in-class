9cf092e53fd9e2f8932047350affc2e52ec072ec
CorinnaK <corinna.kelly@outlook.com>
2020-07-27 12:56:17 -0600

    todo input adds to toDo array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newToDo: "",
+      toDos: [],
+    };
+  }
+  addToDo = (event) => {
+    event.preventDefault();
+    const newTask = {
+      uniqueId: uuidv4(),
+      value: this.state.newToDo,
+    };
+    const currentToDoList = [...this.state.toDos];
+    currentToDoList.push(newTask);
+    this.setState({
+      toDos: currentToDoList,
+      newToDo: "",
+    });
+  };
+
+  updateItem(key, value) {
+    this.setState({ [key]: value });
+  }
+
+  render() {
+    return (
+      <>
+        <h1> React To-Do App</h1>
+        <form onSubmit={this.addToDo}>
+          <label htmlFor="newToDo">
+            Enter a new "To-Do:"
+            <input
+              type="text"
+              name="newToDo"
+              id="newToDo"
+              required
+              value={this.state.newToDo}
+              onChange={(event) =>
+                this.updateItem("newToDo", event.target.value)
+              }
+            />
+          </label>
+          <input type="submit" value="Add New To-Do" />
+        </form>
+        <h2>Current To-Dos:</h2>
+        <ul></ul>
+      </>
+    );
+  }
 }
 
 export default App;
